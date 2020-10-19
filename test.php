057cb252dbcf09ece20902f65953f5003f97e169
Nadav Ben Haim [Microsoft] <nadavbh@microsoft.com>
2020-10-19 13:20:00 +0000

    add additional configuration details

--- test.php
+++ test.php
@@ -5,7 +5,7 @@
  <body>
 <body bgcolor="#f1f1f1" text="Blue">
 <img src="https://nadavstrg.blob.core.windows.net/public/docker-gif-.gif" alt="Docker">
-<h1>If you're seeing this, your website works! Try dancing like Will Smith</h1>
+<h1>If you're seeing this, your website works!</h1>
 <?php
 echo "<br>";
 echo 'IP Address is ';
@@ -14,6 +14,8 @@ echo $_SERVER['REMOTE_ADDR'];
 echo "<br>";
 echo 'Your browser is ';
 echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo 'This webpage is powered by Azure Kubernetes Service 1.17.9 and is deployed by GitHub Actions ';
 echo "<br>";
 
 ?> 
